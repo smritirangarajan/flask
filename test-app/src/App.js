e3c563a5cf8ae5d0233d74e26c8dfd4f9d368a1b
smritirangarajan <smriti_rangarajan@berkeley.edu>
2025-04-24 16:36:19 -0700

    Display quotes from Flask backend

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    fetch("https://flask-backend-waqq.onrender.com/api/quote")
+      .then((res) => res.json())
+      .then((data) => setQuote(data.quote))
+      .catch((err) => console.error("Error fetching quote:", err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Quote</h1>
+      <p>{quote}</p>
     </div>
   );
 }
